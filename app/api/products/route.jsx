c8a967e0395c60a50641cdfd27f8cf8b11923d44
ziljnk <linhnd2805@gmail.com>
2025-04-07 15:05:57 +0700

    fix: update Cloudinary configuration to use non-public environment variables

--- app/api/products/route.jsx
+++ app/api/products/route.jsx
@@ -6,8 +6,8 @@ import { currentUser } from "@clerk/nextjs/server";
 import cloudinary from "cloudinary";
 
 cloudinary.v2.config({
-	cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
-	api_key: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
+	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+	api_key: process.env.CLOUDINARY_API_KEY,
 	api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
